2adfe3467e7c8f2925e7207bada5d9b179e622e7
Maxim <mma29121983@gmail.com>
2015-01-20 22:42:33 +0300

    Update board.js

--- app/js/mainApp/directives/board.js
+++ app/js/mainApp/directives/board.js
@@ -4,7 +4,7 @@
             return {
                 restrict: 'A',
                 replace: true,
-                templateUrl: "js/mainApp/views/board.html",
+                templateUrl: "app/js/mainApp/views/board.html",
                 controller: ['$scope','dataModel','serverModel', ctrlHandler]
             };
 
